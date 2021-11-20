b36c64f2db099e9ef05aaab90e06a2df81120701
Scenaristeur <scenaristeur@gmail.com>
2021-11-20 20:44:38 +0100

    basic add props fields tabs

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -21,54 +21,92 @@
 
     <b-row v-for="p in node.properties" :key="p.name">
       <b-col sm="3">
-        <label for="name">{{p.name}}</label>
+        <label for="name"><code>{{p.name}}</code>:</label>
       </b-col>
       <b-col sm="9">
 
- <b-button-toolbar key-nav aria-label="Toolbar with button groups">
-        <!-- <b-button-group size="sm" variant="outline-primary" class="mr-1">
-    <b-button>Save</b-button>
-    <b-button>Cancel</b-button>
-    <b-button @click="fieldType = 'text'">Text</b-button>
-    <b-button @click="fieldType = 'textarea'">TextArea</b-button>
-    <b-button @click="fieldType = 'link'">Link</b-button>
-  </b-button-group> -->
-  <b-dropdown size="sm" class="mx-1" right text="+" variant="outline-primary">
-     <b-dropdown-item @click="fieldType = 'text'">Text</b-dropdown-item>
-     <b-dropdown-item @click="fieldType = 'textarea'">Textarea</b-dropdown-item>
-     <b-dropdown-item @click="fieldType = 'node'">Node</b-dropdown-item>
-     <b-dropdown-item @click="fieldType = 'link'">Link</b-dropdown-item>
-   </b-dropdown>
- </b-button-toolbar>
+        <!-- <b-button-toolbar key-nav aria-label="Toolbar with button groups">
+        <b-dropdown size="sm" class="mx-1" right text="+" variant="outline-primary">
+        <b-dropdown-item @click="fieldType = 'text'">Text</b-dropdown-item>
+        <b-dropdown-item @click="fieldType = 'textarea'">Textarea</b-dropdown-item>
+        <b-dropdown-item @click="fieldType = 'node'">Node</b-dropdown-item>
+        <b-dropdown-item @click="fieldType = 'link'">Link</b-dropdown-item>
+      </b-dropdown>
 
- {{ p.values}}
-{{fieldType}}
-        <!-- <b-form-input id="name" v-model="node.name" required /> -->
-      </b-col>
-    </b-row>
+    </b-button-toolbar> -->
+    <b-button @click="showFieldModal(p)" variant="outline-primary">+</b-button>
+    {{ p.values}}
 
-    <b-row v-if="field != null" class="mt-3">
-      <b-col sm="3">
-        <b-form-input @change="fieldNameChanged" v-model="field.name" required placeholder="property name" />
-      </b-col>
-      <b-col sm="9">
-        <b-button variant="outline-secondary" size="sm" @click="clear_field">X</b-button>
-      </b-col>
-    </b-row>
+    <!-- <b-form-input id="name" v-model="node.name" required /> -->
+  </b-col>
+</b-row>
 
-    <b-row>
-      <b-col>
-      </b-col>
-      <b-col>
-        <b-btn variant="outline-primary" size="sm" @click="add">+ add property or link</b-btn>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <b-btn variant="success" @click="save">Save Node</b-btn>
-      </b-col>
-    </b-row>
-  </b-container>
+<b-row v-if="field != null" class="mt-3">
+  <b-col sm="3">
+    <b-form-input @change="fieldNameChanged" v-model="field.name" required placeholder="property name" />
+  </b-col>
+  <b-col sm="9">
+    <b-button variant="outline-secondary" size="sm" @click="clear_field">X</b-button>
+  </b-col>
+</b-row>
+
+<b-row>
+  <b-col>
+  </b-col>
+  <b-col>
+    <b-btn variant="outline-primary" size="sm" @click="add">+ add property or link</b-btn>
+  </b-col>
+</b-row>
+<b-row>
+  <b-col>
+    <b-btn variant="success" @click="save">Save Node</b-btn>
+  </b-col>
+</b-row>
+
+<b-modal id="fieldModal" size="xl" :title="node.name+' -> '+currentProp.name">
+{{ currentProp}}
+  <b-tabs content-class="mt-3">
+    <b-tab title="text" active @click="fieldType = 'text'">
+      <b-form-input
+      v-model="newvalue"
+      placeholder="new value"
+      @change="addNewValue"/>
+    </b-tab>
+    <b-tab title="textarea" @click="fieldType = 'textarea'">
+      <b-form-textarea
+      v-model="newvalue"
+      placeholder="Enter something..."
+      rows="3"
+      max-rows="6"
+      @change="addNewValue"
+    ></b-form-textarea>
+    </b-tab>
+    <b-tab title="node" @click="fieldType = 'node'">
+      <b-form-input
+      v-model="newvalue"
+      placeholder="new node todo"
+      @change="addNewValue"/>
+    </b-tab>
+    <b-tab title="link" @click="fieldType = 'link'">
+      <b-form-input
+      v-model="newvalue"
+      placeholder="new link todo"
+      @change="addNewValue"/>
+    </b-tab>
+    <b-tab title="markdown" disabled @click="fieldType = 'markdown'">
+      <b-form-textarea
+      id="textarea"
+      v-model="newvalue"
+      placeholder="Enter something..."
+      rows="3"
+      max-rows="6"
+      @change="addNewValue"
+    ></b-form-textarea>
+    </b-tab>
+  </b-tabs>
+</b-modal>
+
+</b-container>
 </template>
 
 <script>
@@ -80,7 +118,10 @@ export default {
       node:null,
       field : null,
       clearing: false,
-      fieldType: null
+      fieldType: "text",
+      show: false,
+      currentProp: {},
+      newvalue: null
     }
   },
   created() {
@@ -111,6 +152,16 @@ export default {
       this.clearing = true
       this.field = null
       this.clearing = false
+    },
+    showFieldModal(p){
+      console.log(p)
+      this.currentProp = p
+      this.$bvModal.show("fieldModal")
+    },
+    addNewValue(){
+      let val = {value: this.newvalue,  type: this.fieldType}
+      this.currentProp.values.push(val)
+      this.newvalue = ""
     }
   }
 };
