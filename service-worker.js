6cb28cd0e7594597b29b78d02c8255db0a0a037a
Scenaristeur <scenrister@galcm>
2021-11-21 22:43:43 +0100

    sync & subscribe

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/verse/precache-manifest.8059ac79ebd9518a6bdb295636e318d6.js"
+  "/verse/precache-manifest.800c1b93e7d3e0fce8e968d1a1d9f76a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "verse"});
